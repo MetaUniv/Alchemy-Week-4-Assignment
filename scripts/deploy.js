97fff7b36a02bdcc656b3692e583f320baaf5597
MetaUniv <anandbanavath2@gmail.com>
2023-03-31 13:59:02 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,8 +14,13 @@ async function main() {
   await eventWinner.callWinner(winnerContractAddress);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+//Deployed Event Winner Contract Address 
+// https://goerli.etherscan.io/address/0xb7e309973cd06895aae8cb899ffea991db4eb853
+
+//Winning Attempt Trasaction
+//https://goerli.etherscan.io/tx/0x29ea706d3d4d45fd9d4c6b66f314e330b65aa63d77aa0739725a26e6e38767ed
+
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
